74f0eb1987132e40e832887d8c607f23fab7fa9f
Sander Beekhuis <sander.beekhuis@live.nl>
2016-09-20 22:37:57 +0200

    Initial commit

---
+++ Thesis.mm
@@ -0,0 +1,88 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1473773859556" ID="ID_1983158590" MODIFIED="1474126472684" TEXT="Thesis">
+<node CREATED="1473774041237" ID="ID_1567848454" MODIFIED="1473774144084" POSITION="right" TEXT="Find and read thesis of Vincent kusters"/>
+<node CREATED="1473774222805" ID="ID_881835839" MODIFIED="1473958051482" POSITION="right" TEXT="First attempt at an algo (some constant, many)">
+<node CREATED="1473774332524" ID="ID_1984983" MODIFIED="1473774388768" TEXT="Proof formally: All vert/All horz exists"/>
+<node CREATED="1473774257838" ID="ID_1674617395" MODIFIED="1473774317527" TEXT="Sweep line algo for  all vert one-sided"/>
+<node CREATED="1473774319086" ID="ID_61844726" MODIFIED="1473774331312" TEXT="Sweep line algo for all hor one-sided"/>
+<node CREATED="1473774391170" ID="ID_59443853" MODIFIED="1473774412198" TEXT="Look how red screws up blue"/>
+<node CREATED="1473958053507" ID="ID_1947205521" MODIFIED="1473958068670" TEXT="Eligible paths covering two neighbouring vertices?"/>
+</node>
+<node CREATED="1473774466811" ID="ID_157385896" MODIFIED="1473957868562" POSITION="right" TEXT="Lower bound (2, inf)">
+<node CREATED="1473774452187" FOLDED="true" ID="ID_575345704" MODIFIED="1473957871338" TEXT="Fix corner assignment using a scafold">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1473842745259" ID="ID_1630256384" MODIFIED="1473957860922" TEXT="What does the inside of a seperating 4-cycle look like?">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+<node CREATED="1473774503984" ID="ID_1393785544" MODIFIED="1473782842332" POSITION="right" TEXT="Forms">
+<node CREATED="1473774511715" ID="ID_937272477" MODIFIED="1473782755657" TEXT="Write problem description">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1473774521902" ID="ID_1970355245" MODIFIED="1473774533538" TEXT="Get sign of jesper"/>
+</node>
+<node CREATED="1473774556111" ID="ID_845232688" MODIFIED="1473958737446" POSITION="right" TEXT="Interior of a seperating cycle is totally red/blue in one corner">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Page 1
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1473774748123" ID="ID_478412164" MODIFIED="1473774757980" POSITION="right" TEXT="Complete Fusy correctnes proof">
+<node CREATED="1473774770240" ID="ID_1119133447" MODIFIED="1473774781870" TEXT="Compare fusy papers">
+<node CREATED="1473774783545" ID="ID_919429220" MODIFIED="1473774794362" TEXT="eligible path properties"/>
+<node CREATED="1473774795672" ID="ID_1808042557" MODIFIED="1473774800778" TEXT="cycle invariant"/>
+</node>
+<node CREATED="1473774869578" ID="ID_746476716" MODIFIED="1473774879209" TEXT="Understanding eligible paths">
+<node CREATED="1473774145224" ID="ID_916704983" MODIFIED="1473774944504" TEXT="Can a shortcut later hit an eligible path(That is instead of at the first node)"/>
+<node CREATED="1473774705617" ID="ID_1720207930" MODIFIED="1473774880732" TEXT="Provide example graphs show all requirements of an eliggble path are necessary"/>
+<node CREATED="1473774883467" ID="ID_878035813" MODIFIED="1473775194234" TEXT="In oriented contex: At a node at most one eligble path can start/stop">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Page2
+    </p>
+  </body>
+</html></richcontent>
+</node>
+<node CREATED="1473775236612" ID="ID_812676911" MODIFIED="1473775355269" TEXT="When does no ePath start at a vertex">
+<richcontent TYPE="NOTE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Page 3
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+</node>
+<node CREATED="1473842821586" ID="ID_1246127513" MODIFIED="1474277338130" POSITION="left" TEXT="Questions">
+<node CREATED="1473842835403" ID="ID_1042196915" MODIFIED="1473842851219" TEXT="Is there a difference between SIAM and arxiv eppstein et al."/>
+<node CREATED="1473859715699" ID="ID_350240303" MODIFIED="1473859749807" TEXT="Is copying a definition plagiaat (e.g rectangular layout)?"/>
+<node CREATED="1473929319202" ID="ID_1685192408" MODIFIED="1473929478181" TEXT="Proof. In any layout dual to E(G), the region enclosed by the four rectangles of the separating cycle C must be a four-sided polygon, that is, a rectangle.  ~Eppstein et al, Lemma 5 (Why is this true?, see also Page 4)"/>
+<node CREATED="1473931269390" ID="ID_526559619" MODIFIED="1474371885079" TEXT="Writing">
+<node CREATED="1473931279761" ID="ID_1988052979" MODIFIED="1473931290553" TEXT="Corner assignment vs extension"/>
+<node CREATED="1474123094102" ID="ID_910591816" MODIFIED="1474123168262" TEXT="For triangulations of the $k$-gon, should a speak of a maximally planar interior of faces of degree 3? The first seems more clear while the second is less ambigious"/>
+<node CREATED="1474126984856" ID="ID_1107650061" MODIFIED="1474127002273" TEXT="Interior vs inner vertex against outer vertex"/>
+</node>
+<node CREATED="1473958351956" ID="ID_686055515" MODIFIED="1473958385722" TEXT="Is it usefull to provide the corner assignment argument stand-alone?"/>
+</node>
+<node CREATED="1473958082232" ID="ID_1714315851" MODIFIED="1473958092028" POSITION="left" TEXT="Look at alpha_0 orientations"/>
+<node CREATED="1474126475029" ID="ID_783889295" MODIFIED="1474126481856" POSITION="left" TEXT="Versioning my thesis"/>
+</node>
+</map>
