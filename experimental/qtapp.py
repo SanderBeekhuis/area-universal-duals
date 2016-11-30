d757987abc34e5ff31b091ca08e3f75c510fe751
Sander Beekhuis <sander.beekhuis@live.nl>
2016-11-30 18:11:31 +0100

    more work on qtapp

--- experimental/qtapp.py
+++ experimental/qtapp.py
@@ -2,18 +2,14 @@
 # -*- coding: utf-8 -*-
 
 """
-ZetCode PyQt4 tutorial
+Todo
 
-In this example we draw 6 lines using
-different pen styles.
+- Change color
 
-author: Jan Bodnar
-website: zetcode.com
-last edited: September 2011
 """
 
 import sys
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QPainter, QColor, QFont, QPen, QBrush
 import networkx as nx
@@ -27,17 +23,36 @@ VERTEXWIDTH = 5
 
 
 
-def eventDistance
+def coordDistanceSq(c, e):
+    return (c.x - e.x)**2 + (c.y - e.y)**2
+
+
+class Main(QWidget):
+    def __init__(self):
+        super(Main, self).__init__()
+        self.initUI()
+
+    def initUI(self):
+
+        self.setGeometry(300, 300, 280, 270)
+        self.setWindowTitle('Graph Shower')
+
+        but = QPushButton('Save', self)
+        but.move(10,10)
+
+        canv = Canvas(self)
+        canv.move(10, 50)
+
+        self.show()
+
 
 
 class Canvas(QWidget):
 
-    def __init__(self):
-        super(Example, self).__init__()
-
+    def __init__(self, parent):
+        super(Canvas, self).__init__(parent)
         self.graph  = self.initGraph()
         self.leftdown = None
-        self.initUI()
 
     def initGraph(self):
         G = nx.Graph()
@@ -46,25 +61,52 @@ class Canvas(QWidget):
         G.add_node(v1)
         G.add_node(v2)
         G.add_edge(v1,v2, color='red')
-        print(G.nodes())
-        print(G.edges())
-
+        # print(G.nodes())
+        # print(G.edges())
         return G
 
-    def initUI(self):
+    def closestNode(self, coord):
+        distance = 10000000
+        candidate = None
+        for n in self.graph.nodes():
+            if coordDistanceSq(n, coord) < distance:
+                candidate = n
+                distance = coordDistanceSq(n,coord)
+        if distance < 100:
+            return candidate
 
-        self.setGeometry(300, 300, 280, 270)
-        self.setWindowTitle('Pen styles')
-        self.show()
+
+
 
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
-            self.leftdown = event
+            self.leftdown = Coord(event.x(), event.y())
 
     def mouseReleaseEvent(self, event):
-        if event.button() == Qt.LeftButton and not leftdown is None :
-            if distance(self.leftdown, event) <10:
-                G.add_node
+
+        if event.button() == Qt.LeftButton and not self.leftdown is None :
+            leftup = Coord(event.x(), event.y())
+
+            if coordDistanceSq(self.leftdown, leftup) <10:
+                print("Distance is: {}. Adding node".format(coordDistanceSq(self.leftdown, leftup)))
+                if not closestNode(self.leftdown) is None:
+                    print("To close to an other node. Abort")
+                    self.leftdown=None
+                    return
+
+                self.graph.add_node(self.leftdown)
+                self.leftdown = None
+                self.update()
+
+            else:
+                v1 = self.closestNode(self.leftdown)
+                v2 = self.closestNode(leftup)
+
+                if not v1 is None and not v2 is None:
+                    print("Adding edge")
+                    self.graph.add_edge(v1, v2, color = 'black')
+                    self.leftdown = None
+                    self.update()
 
 
     def paintEvent(self, e):
@@ -77,7 +119,7 @@ class Canvas(QWidget):
 
         redpen = QPen(Qt.red,2)
         bluepen = QPen(Qt.blue,2)
-        blackpen = QPen(2)
+        blackpen = QPen(Qt.black, 2)
         pens={'black':blackpen, 'red':redpen, 'blue':bluepen}
 
 
@@ -98,7 +140,7 @@ class Canvas(QWidget):
 def main():
 
     app = QApplication(sys.argv)
-    ex = Canvas()
+    ex = Main()
     sys.exit(app.exec_())
 
 
