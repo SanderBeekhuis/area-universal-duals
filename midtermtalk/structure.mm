20d71b5cf05eab056e6c8a9aba602474229e5fab
Sander Beekhuis <sander.beekhuis@live.nl>
2016-12-04 10:54:01 +0100

    Prepare midtermtalk abstract

---
+++ midtermtalk/structure.mm
@@ -0,0 +1,69 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Midterm Pres (30 min)" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1480843872408"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5"/>
+<node TEXT="Setting" POSITION="right" ID="ID_1192169832" CREATED="1480843851562" MODIFIED="1480843885558">
+<edge COLOR="#ff0000"/>
+<node TEXT="Graph and Rectangular dual" ID="ID_1183850944" CREATED="1480843897195" MODIFIED="1480843906118"/>
+<node TEXT="Correspondece to REL" ID="ID_383033393" CREATED="1480843906982" MODIFIED="1480843912525"/>
+<node TEXT="What is a pseudo onesided layout" ID="ID_63673093" CREATED="1480843991002" MODIFIED="1480844016702"/>
+</node>
+<node TEXT="Computing A REL" POSITION="right" ID="ID_426466656" CREATED="1480843886547" MODIFIED="1480843944260">
+<edge COLOR="#0000ff"/>
+<node TEXT="Fusy&apos;s Algorithm" ID="ID_1064813609" CREATED="1480843945540" MODIFIED="1480843950523"/>
+</node>
+<node TEXT="Results" POSITION="right" ID="ID_891407291" CREATED="1480843976976" MODIFIED="1480844171543">
+<edge COLOR="#00ffff"/>
+<node TEXT="Seperating 4-cycles are  bad" ID="ID_1263730603" CREATED="1480844132548" MODIFIED="1480844171542"/>
+<node TEXT="Progress on finding an algorithm" ID="ID_893324210" CREATED="1480844172999" MODIFIED="1480844185043"/>
+</node>
+</node>
+</map>
