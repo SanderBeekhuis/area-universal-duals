05e1aa2bc7de4146dab04ef990aef53d7e0206e0
Sander Beekhuis <sander.beekhuis@live.nl>
2017-02-25 18:00:38 +0100

    added mindmaps for syory chapters

---
+++ thesis/conclusion/Conclusion and future work.mm
@@ -0,0 +1,62 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Conclusion and future work" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1488037081543"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="2"/>
+<node TEXT="Summarize results" POSITION="right" ID="ID_269676474" CREATED="1488037094273" MODIFIED="1488037111515">
+<edge COLOR="#ff0000"/>
+</node>
+<node TEXT="State what we hope to be true" POSITION="right" ID="ID_622551836" CREATED="1488037111973" MODIFIED="1488037121274">
+<edge COLOR="#0000ff"/>
+<node TEXT="There is an algortihm that gives d-sided layouts for separating 4-cycles" ID="ID_1637690137" CREATED="1488037134614" MODIFIED="1488037159096"/>
+<node TEXT="Without separating 4-cycles we hope actually all graphs are 2-sided" ID="ID_1106153334" CREATED="1488037159517" MODIFIED="1488037204620"/>
+</node>
+</node>
+</map>
---
+++ thesis/introduction/Introduction structure.mm
@@ -0,0 +1,93 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Introduction" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1488036404956"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="8"/>
+<node TEXT="Cartograms" POSITION="right" ID="ID_1244266734" CREATED="1488036406084" MODIFIED="1488036412285">
+<edge COLOR="#ff0000"/>
+<node TEXT="What are they?" ID="ID_469385846" CREATED="1488036415731" MODIFIED="1488036419266"/>
+</node>
+<node TEXT="Change data" POSITION="right" ID="ID_380053497" CREATED="1488036422734" MODIFIED="1488036430335">
+<edge COLOR="#0000ff"/>
+<node TEXT="Resizing the rectangles in a cartogram" ID="ID_572150647" CREATED="1488036431646" MODIFIED="1488036443295"/>
+<node TEXT="But we want to keep layout" ID="ID_882820607" CREATED="1488036458256" MODIFIED="1488036469157">
+<node TEXT="That is an equivalent layout," ID="ID_1466865020" CREATED="1488036620872" MODIFIED="1488036631304">
+<node TEXT="However we can only do order equiv." ID="ID_184940750" CREATED="1488036634837" MODIFIED="1488036649681"/>
+</node>
+</node>
+<node TEXT="So one-sided layouts are great" ID="ID_770171906" CREATED="1488036488399" MODIFIED="1488036497507"/>
+<node TEXT="Unfortuantly not all graphs are one-sided" ID="ID_1089265023" CREATED="1488036498028" MODIFIED="1488036508236">
+<node TEXT="e.g. Rinsma" ID="ID_334732792" CREATED="1488036543727" MODIFIED="1488036549952"/>
+</node>
+</node>
+<node TEXT="ks-ded layouts are the next best thing" POSITION="right" ID="ID_206601973" CREATED="1488036445801" MODIFIED="1488036525946">
+<edge COLOR="#00ff00"/>
+<node TEXT="They also encounter hurles" ID="ID_1513003302" CREATED="1488036527719" MODIFIED="1488036541660"/>
+<node TEXT="This are seperating 4-cycles" ID="ID_1804653721" CREATED="1488036558694" MODIFIED="1488036570206">
+<node TEXT="These are also annoying in the slicable case" ID="ID_1961281555" CREATED="1488036572493" MODIFIED="1488036585648"/>
+<node TEXT="But not so much for the area-universal story" ID="ID_631411549" CREATED="1488036586259" MODIFIED="1488036599613"/>
+</node>
+<node TEXT="It seems 4-cycles sharing vertices are the worst" ID="ID_797960501" CREATED="1488036889293" MODIFIED="1488036908403"/>
+</node>
+<node TEXT="Horrizatal one-sidedness is easy" POSITION="right" ID="ID_1898841242" CREATED="1488037040334" MODIFIED="1488037048952">
+<edge COLOR="#7c0000"/>
+</node>
+<node TEXT="Discuss results" POSITION="right" ID="ID_1803744091" CREATED="1488036920659" MODIFIED="1488036930255">
+<edge COLOR="#ff00ff"/>
+<node TEXT="The algorithm vs the counterexample" ID="ID_1174846501" CREATED="1488036934750" MODIFIED="1488036949399">
+<node TEXT="That is the counter" ID="ID_833490964" CREATED="1488036953774" MODIFIED="1488037033908"/>
+</node>
+</node>
+<node TEXT="Nice phrases" POSITION="left" ID="ID_513994857" CREATED="1488038320282" MODIFIED="1488038326372">
+<edge COLOR="#00007c"/>
+<node TEXT="&quot;Roughly speaking," ID="ID_1224772051" CREATED="1488038327615" MODIFIED="1488040419103"/>
+</node>
+</node>
+</map>
